4ae8c2d15d806887d9e2def545f8d7b23509d475
alisalali <alis.alali@hotmail.com>
2023-10-24 21:51:53 +0300

    inline style

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -3,25 +3,40 @@ import "./CourseInput.css";
 import Button from "../UI/Button";
 export const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
     //binding enteredText to app components with prop onAddGoal
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
     setEnteredValue("");
+    // setIsValid(true);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goals</label>
+        <label style={{ color: !isValid ? "red" : "black" }}>
+          Course Goals
+        </label>
         <input
           type="text"
           value={enteredValue}
+          style={{
+            borderColor: !isValid ? "red" : "black",
+            background: !isValid ? "salmon" : "transparent",
+          }}
           onChange={goalInputChangeHandler}
         ></input>
       </div>
